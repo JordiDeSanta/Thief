c3ae24170feb8937d3ce7fbb1fe86fcdb1b56550
Jordi <jordi.bravo.bad@gmail.com>
2020-08-27 19:17:07 -0400

    Renaming variables to UE4 convention

--- triplex.cpp
+++ triplex.cpp
@@ -6,20 +6,20 @@ int main()
     // Intro
     std::cout << "You're a thief trying steal a house\n";
     std::cout << "Desactive the alarm or be exposed and enjailed\n";
-    std::cout << "Discover the 3 numbers key..\n" << "Hints:\n";
+    std::cout << "Discover the 3 numbers key..\n\n" << "Hints:\n";
     
     // Declare correct key and empty input key
-    const int realKey[3] = {2, 2, 3};
+    const int RealKey[3] = {2, 2, 3};
     int key[3];
 
     // Calculate sum and product of the digits to give in the hint
-    const int sum = realKey[0] + realKey[1] + realKey[2];
-    const int mul = realKey[0] * realKey[1] * realKey[2];
+    const int KeySum = RealKey[0] + RealKey[1] + RealKey[2];
+    const int KeyMul = RealKey[0] * RealKey[1] * RealKey[2];
 
     // Print hints
-    std::cout << "+ The product of this 3 numbers is " << mul << std::endl;
-    std::cout << "+ The sum of this 3 numbes is " << sum << std::endl;
-    std::cout << "+ Extra: the order is from lowest to highest\n";
+    std::cout << "+ The product of this 3 numbers is " << KeyMul << std::endl;
+    std::cout << "+ The sum of this 3 numbes is " << KeySum << std::endl;
+    std::cout << "+ Extra: the order is from lowest to highest\n\n";
 
     // For loop what contains user input for once digit of the password
     for (int i = 0; i < 3; i++)
@@ -28,15 +28,16 @@ int main()
         std::cin >> key[i]; // Ask to player the i digit of password
 
         // if the player guess a number the alarm call the polica
-        if (key[i] != realKey[i])
+        if (key[i] != RealKey[i])
         {
             // Game Over
             std::cout << "Police!, hands up";
             return 0;
-        }
-    }
+        };
+    };
     
     // You Win!
     std::cout << "*deactivated alarm sound";
+
     return 0;
 }   
