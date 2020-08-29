3a16f067ecaa92915c1c00b477d86f734b728d21
Jordi <jordi.bravo.bad@gmail.com>
2020-08-29 17:20:51 -0400

    Finished project

--- thief.cpp
+++ thief.cpp
@@ -28,16 +28,18 @@ void Intro(int Dif)
 void Hints(int Mul, int Sum)
 {
     // Print hints
-    std::cout << "+ The product of this 3 numbers is " << Mul << std::endl;
-    std::cout << "+ The sum of this 3 numbes is " << Sum << std::endl << std::endl;
+    std::cout << "+ The product of them numbers is " << Mul << std::endl;
+    std::cout << "+ The sum of them is " << Sum << std::endl << std::endl;
 };
 
 bool PlayGame(int LevelDifficulty)
 {
     Intro(LevelDifficulty);
 
+    int m = (3 * LevelDifficulty + 1);
+
     // Declare correct key and empty input key
-    const int RealKey[3] = {2, 2, 3};
+    const int RealKey[3] = {rand() % m, rand() % m, rand() % m};
     int Key[3];
 
     // Calculate sum and product of the digits to give in the hint
@@ -60,7 +62,7 @@ bool PlayGame(int LevelDifficulty)
     GuessMul = Key[0] * Key[1] * Key[2];
 
     // If the hints agree with the player reply
-    if(GuessSum == KeySum && GuessMul == KeyMul)
+    if (GuessSum == KeySum && GuessMul == KeyMul)
     {
         std::cout << "\n*deactivated alarm sound\n\n"; // You Win!
         return true;
@@ -75,28 +77,23 @@ bool PlayGame(int LevelDifficulty)
 int main() 
 {
     // Necessary parameters
-    int NumberOfLevels = 3;
+    const int NumberOfLevels = 3;
     int Difficulty = 1;
 
     // Game Loop
-    while (true)
+    while (Difficulty <= NumberOfLevels)
     {
         // Start level
         bool bLevelCompleted = PlayGame(Difficulty);
         std::cin.clear(); // Clears any errors
         std::cin.ignore(); // Discards the buffer
 
-        // Level pass system
-        if(bLevelCompleted)
+        if (bLevelCompleted)
         {
             ++Difficulty;
-            //            
-            if (Difficulty > NumberOfLevels)
-            {
-                return 0;
-            };
         };
-    }
-  
+    };
+
+    std::cout << "DAMN YOU'RE A GENIUS OF STEAL\n";
     return 0;
 };  
