fe809e46ea5775f237e7194af299aa6d2ffca78b
Jordi <jordi.bravo.bad@gmail.com>
2020-08-29 16:55:33 -0400

    Level pass system

--- triplex.cpp
+++ triplex.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <string>
 
-
-void Intro()
+void Intro(int Dif)
 {
+    std::string House;
+
+    // Define the type of house depending of the difficult level
+    switch (Dif)
+    {
+    case 1: // Level 1, Little
+        House = "poor";
+        break;
+    case 2: // Level 2, Medium
+        House = "medium class";
+        break;
+    case 3: // Level 3, Big
+        House = "rich";
+        break;
+    };
+
     // Intro
-    std::cout << "You're a thief trying steal a house\n";
+    std::cout << "You're a thief trying steal a " + House + " house\n";
     std::cout << "Desactive the alarm or be exposed and enjailed\n";
     std::cout << "Discover the 3 numbers key..\n\n" << "Hints:\n";
 };
@@ -16,9 +32,9 @@ void Hints(int Mul, int Sum)
     std::cout << "+ The sum of this 3 numbes is " << Sum << std::endl << std::endl;
 };
 
-bool PlayGame()
+bool PlayGame(int LevelDifficulty)
 {
-    Intro();
+    Intro(LevelDifficulty);
 
     // Declare correct key and empty input key
     const int RealKey[3] = {2, 2, 3};
@@ -58,13 +74,28 @@ bool PlayGame()
 
 int main() 
 {
-    bool bLevelCompleted = false;
+    // Necessary parameters
+    int NumberOfLevels = 3;
+    int Difficulty = 1;
 
-    while (!bLevelCompleted)
+    // Game Loop
+    while (true)
     {
-        bLevelCompleted = PlayGame();
+        // Start level
+        bool bLevelCompleted = PlayGame(Difficulty);
         std::cin.clear(); // Clears any errors
         std::cin.ignore(); // Discards the buffer
+
+        // Level pass system
+        if(bLevelCompleted)
+        {
+            ++Difficulty;
+            //            
+            if (Difficulty > NumberOfLevels)
+            {
+                return 0;
+            };
+        };
     }
   
     return 0;
